5ed0d039e479918d42aef969c30be72dd5d4554f
tanja-hr <tanja.hromin@gmail.com>
2023-09-04 00:07:05 +0200

    updated regex

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,13 @@
+/**
+ * ME - ISO country code
+ * 2 digits - IBAN check digits
+ * 3 digits - bank code
+ * 13 digits - account number
+ * 2 digits - check digits
+ */
+
 export const validateIBAN = (iban: string): boolean => {
-    const regex = new RegExp(/^ME\d{20}$/);
+    const regex = new RegExp(/^ME\d{2}\d{3}\d{13}\d{2}$/);
     const cleanedIBAN = iban.replace(/\s+/g, "").toUpperCase();
     return regex.test(cleanedIBAN);
 };
