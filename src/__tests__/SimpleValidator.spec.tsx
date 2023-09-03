0e038249cfd76bdcc7deef35d45454cf84ff1a11
tanja-hr <tanja.hromin@gmail.com>
2023-09-03 20:43:18 +0200

    added test

--- src/__tests__/SimpleValidator.spec.tsx
+++ src/__tests__/SimpleValidator.spec.tsx
@@ -4,15 +4,21 @@ import SimpleValidator from "../SimpleValidator";
 
 describe("<SimpleValidator />", () => {
     it("displays the result on button press", () => {
-        const { getByTestId, getByPlaceholderText } = render(<SimpleValidator />);
-
-        const inputField = getByPlaceholderText("MN...");
-        fireEvent.changeText(inputField, "text");
-
+        const { getByTestId } = render(<SimpleValidator />);
         const button = getByTestId("validate-button");
         fireEvent.press(button);
         const result = getByTestId("result");
 
         expect(result).toBeDefined();
+    });
+
+    it("hides the result on input change", () => {
+        const { queryByTestId, getByPlaceholderText } = render(<SimpleValidator />);
+        const inputField = getByPlaceholderText("MN...");
+        fireEvent.changeText(inputField, "text");
+
+        const result = queryByTestId("result");
+
+        expect(result).toBeNull();
     });
 });
